055ccdeb3c6c66f764d66e298415eca45a07b10d
Christopher Banaszak <ludachris@Christophers-MacBook-Pro.local>
2017-12-19 18:17:25 -0600

    Final version

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,6 +7,13 @@ $(document).ready(function(){
 
 	var sum = 0;
 	var numValues = [2, 6, 10, 4];
+	var wins = 0;
+	$("#wins").text(wins);
+
+	var losses = 0;
+	$("#losses").text(wins);
+
+	$("#current-sum").text(sum);
 
 	for(var i = 0; i < numValues.length; i++){
 		var newImg = $("<img>");
@@ -24,17 +31,24 @@ $(document).ready(function(){
 	$("#images").on("click", "img.crystal-img", function(){
 		var numberToAdd = parseInt($(this).attr("numberValue"));
 		sum += numberToAdd;
+		$("#announcement").text("");
 
 		if(sum == randoNumber){
-			alert("You win! Here's a new number.");
+			$("#announcement").text("You win! Here's a new number.");
+			wins++;
+			
+
 			reset();
 		}
 		else if(sum > randoNumber){
-			alert("You lost, sorry! Here's a new number.");
+			$("#announcement").text("You lost! Here's a new number.");
+			losses++;
+			
+
 			reset();
 		}
 		else{
-			alert("Your current sum is: " + sum);
+			$("#current-sum").text(sum);
 		}
 	});
 
@@ -42,6 +56,9 @@ $(document).ready(function(){
 		sum = 0;
 		randoNumber = Math.floor(Math.random() * (120-19)) + 19;
 		$("#number_to_guess").text(randoNumber);
+		$("#wins").text(wins);
+		$("#losses").text(losses);
+		$("#current-sum").text(sum);
 		
 		for(var i = 0; i < numValues.length; i++){
 			numValues[i] = Math.floor(Math.random() * 11) + 1;
